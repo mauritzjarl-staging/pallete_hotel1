8b57a4384d9f17c27f7f80162985ff2c16279288
moiz-satti-7 <moiz.satti.885@gmail.com>
2024-09-18 10:27:09 +0500

    Project Completed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,61 @@
 "use client";
-import './globals.css';
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import Header from "./components/header";
+import Footer from "./components/footer";
 import { usePathname } from 'next/navigation';
-import Header from './components/header';
-import Footer from './components/footer';
-import { Montserrat } from 'next/font/google';
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400', '700'], // Adjust as needed
+  subsets: ["latin"],
+  weight: ["400", "700"], // Adjust as needed
 });
 
 export default function ClientOnlyLayout({ children }) {
   const pathname = usePathname();
   const isLoginPage = pathname.startsWith('/login');
+
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/imgs/Pallhotellet.seTransparent.png" />
+        <title>PallHotellet.se</title>
+        <meta name="description" content="PallHotellet Sweden" />
+      </head>
       <body className={montserrat.className}>
-      {!isLoginPage && <Header />}
-      <main>{children}</main>
-      {!isLoginPage && <Footer />}
+        {!isLoginPage && <Header />}
+        <main>{children}</main>
+        {!isLoginPage && <Footer />}
       </body>
     </html>
   );
 }
+
+
+
+
+
+
+// import './globals.css';
+// import { usePathname } from 'next/navigation';
+// import Header from './components/header';
+// import Footer from './components/footer';
+// import { Montserrat } from 'next/font/google';
+
+// const montserrat = Montserrat({
+//   subsets: ['latin'],
+//   weight: ['400', '700'], // Adjust as needed
+// });
+
+// export default function ClientOnlyLayout({ children }) {
+//   const pathname = usePathname();
+//   const isLoginPage = pathname.startsWith('/login');
+//   return (
+//     <html lang="en">
+//       <body className={montserrat.className}>
+//       {!isLoginPage && <Header />}
+//       <main>{children}</main>
+//       {!isLoginPage && <Footer />}
+//       </body>
+//     </html>
+//   );
+// }
