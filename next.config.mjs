7f556a95564a617be5ff4304eca4ee602ceca57f
moiz-satti-7 <moiz.satti.885@gmail.com>
2024-10-02 16:00:29 +0500

    latest

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,56 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        // Redirect all .com domains to the .se domain
-        {
-          source: '/(.*)',
-          has: [{ type: 'host', value: 'www.pallhotellet.com' }],
-          destination: 'https://www.pallhotellet.se/:path*',
-          permanent: true,
-        },
-        {
-          source: '/(.*)',
-          has: [{ type: 'host', value: 'pallhotellet.com' }],
-          destination: 'https://www.pallhotellet.se/:path*',
-          permanent: true,
-        },
-        // Redirect non-www .se domain to the www.pallhotellet.se domain
-        {
-          source: '/(.*)',
-          has: [{ type: 'host', value: 'pallhotellet.se' }],
-          destination: 'https://www.pallhotellet.se/:path*',
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  async redirects() {
+    return [
+      // Redirect all .com domains to the .se domain
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'pallhotellet.se' }],
+        destination: 'https://www.pallhotellet.se/:path*',
+        permanent: true,
+      },
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'www.pallhotellet' }],
+        destination: 'https://www.pallhotellet.se/:path*',
+        permanent: true,
+      },
+      // Redirect non-www .se domain to the www.pallhotellet.se domain
+      {
+        source: '/(.*)',
+        has: [{ type: 'host', value: 'pallhotellet.se' }],
+        destination: 'https://www.pallhotellet.se/:path*',
+        permanent: true,
+      },
+
+      // Redirect user-friendly service URLs to query-based URLs (services)
+      {
+        source: '/services/indoor',
+        destination: '/services?tab=indoor',
+        permanent: true,
+      },
+      {
+        source: '/services/outdoor',
+        destination: '/services?tab=outdoor',
+        permanent: true,
+      },
+      {
+        source: '/services/rental',
+        destination: '/services?tab=rental',
+        permanent: true,
+      },
+      {
+        source: '/services/cargo',
+        destination: '/services?tab=cargo',
+        permanent: true,
+      },
+      {
+        source: '/services/other',
+        destination: '/services?tab=other',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
