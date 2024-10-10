b1d441f0dccbc467be237ba6a0dea063a998a06a
moiz-satti-7 <moiz.satti.885@gmail.com>
2024-10-10 17:46:55 +0500

    latest

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,54 @@
+// // app/page.js
+// import HomeClientComponent from './components/HomeClientComponent';  // Import client-side component
+
+// export async function generateMetadata() {
+//   return {
+    
+//     title: 'Lager & Logistiklösningar i Motela Sverige - Pallhotellet | Lasthanteringsexperter',
+//     description: 'Pålitligt lager i Sverige med flexibel lagring, sakkunnig lasthantering och dokumentationstjänster. Kontakta Pallhotellet för skräddarsydda lösningar.',
+//    alternates: {
+//       canonical: 'https://www.pallhotellet.se/',
+//     },
+//   };
+// }
+
+// export default function HomePage() {
+//   return (
+//       <HomeClientComponent />  
+//   );
+// }
+
+
+
+
+
 // app/page.js
 import HomeClientComponent from './components/HomeClientComponent';  // Import client-side component
 
 export async function generateMetadata() {
+  // Get the host from the request headers
+  const host = typeof window !== 'undefined' ? window.location.host : 'pallhotellet.se'; // Default to .se for server-side rendering
+
+  let canonicalUrl;
+
+  // Determine the canonical URL based on the host
+  if (host.includes('.com')) {
+    canonicalUrl = 'https://www.pallhotellet.com/';
+  } else {
+    canonicalUrl = 'https://www.pallhotellet.se/';
+  }
+
   return {
-    
     title: 'Lager & Logistiklösningar i Motela Sverige - Pallhotellet | Lasthanteringsexperter',
     description: 'Pålitligt lager i Sverige med flexibel lagring, sakkunnig lasthantering och dokumentationstjänster. Kontakta Pallhotellet för skräddarsydda lösningar.',
-   alternates: {
-      canonical: 'https://www.pallhotellet.se/',
+    alternates: {
+      canonical: canonicalUrl,
     },
   };
 }
 
 export default function HomePage() {
   return (
-      <HomeClientComponent />  
+    <HomeClientComponent />  
   );
 }
