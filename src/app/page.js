13c59b3f9a9562480828ccb37ec1a779f3c264aa
Muzdalfa <muzdalfa171@gmail.com>
2024-12-05 16:34:13 +0500

    latest

--- src/app/page.js
+++ src/app/page.js
@@ -1,38 +1,35 @@
 import HomeClientComponent from './components/HomeClientComponent';  // Import client-side component
 
 export async function generateMetadata() {
-  // Get the host from the request headers
-  const host = typeof window !== 'undefined' ? window.location.host : 'pallhotellet.se'; // Default to .se for server-side rendering
-
   let canonicalUrl;
-let title;
+  let title;
   // Determine the canonical URL based on the host
-  if (host.includes('.com')) {
+  if (process.env.DOMAIN_URL.includes('.com')) {
     canonicalUrl = 'https://pallhotellet.com/';
-    title='Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.com';
+    title = 'Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.com';
   }
-  else if('.se') {
+  else if (process.env.DOMAIN_URL.includes('.se')) {
     canonicalUrl = 'https://pallhotellet.se/';
-    title='Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.se';
+    title = 'Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.se';
   }
-  else{
-  // Fallback or default domain handling
-  canonicalUrl = 'https://pallethotellet.com/';
-  title = 'Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.com';
+  else {
+    // Fallback or default domain handling
+    canonicalUrl = 'https://pallethotellet.com/';
+    title = 'Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.com';
   }
 
   return {
-    title: title,
-    
+    title:title,
     description: 'Lagerhållning från 59kr per pall, sakkunnig personal, flexibel lagring, lasthantering och dokumentationstjänster. Kontakta oss på Pallhotellet för skräddarsydda lösningar.',
     alternates: {
       canonical: canonicalUrl,
+    
     },
   };
 }
 
 export default function HomePage() {
   return (
-    <HomeClientComponent />  
+    <HomeClientComponent />
   );
 }
