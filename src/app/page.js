e2d28c2bd8efc7d095b030db0035b3114b95c2d7
Muzdalfa <muzdalfa171@gmail.com>
2024-12-05 16:09:57 +0500

    title update

--- src/app/page.js
+++ src/app/page.js
@@ -5,16 +5,18 @@ export async function generateMetadata() {
   const host = typeof window !== 'undefined' ? window.location.host : 'pallhotellet.se'; // Default to .se for server-side rendering
 
   let canonicalUrl;
-
+let title;
   // Determine the canonical URL based on the host
   if (host.includes('.com')) {
     canonicalUrl = 'https://pallhotellet.com/';
+    title='Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.com';
   } else {
     canonicalUrl = 'https://pallhotellet.se/';
+    title='Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.se';
   }
 
   return {
-    title: 'Logistik- och lagerlösningar i Motala, Östergötland - Pallhotellet.se ',
+    title: title,
     
     description: 'Lagerhållning från 59kr per pall, sakkunnig personal, flexibel lagring, lasthantering och dokumentationstjänster. Kontakta oss på Pallhotellet för skräddarsydda lösningar.',
     alternates: {
